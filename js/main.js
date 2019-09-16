cbb6688acc9d9f6749eb8c8f865419cf1067e4cd
a.suprun <oxymoron.dr@gmail.com>
2019-09-16 13:38:01 +0300

    final

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function() {
 
     $('select').styler();
 
-    $('header__btn-menu').on('click', function () {
+    $('.header__btn-menu').on('click', function() {
         $('.menu ul').slideToggle();
     });
 });
